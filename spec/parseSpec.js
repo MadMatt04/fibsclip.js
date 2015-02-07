c1c9f7652d405a71e0cc6934209f8a4e1ea72458
Matija Kejzar <matija.kejzar@marand.si>
2015-02-07 19:50:41 +0100

    Parsing - 1 CLIP Welcome

--- spec/parseSpec.js
+++ spec/parseSpec.js
@@ -13,4 +13,27 @@ describe("fibsclip Parsing Specs", function () {
         var sequence = ar2uinta([255, 252, 1, 13, 10, 108, 111, 103, 105, 110, 58, 32]);
         fibs.parse(sequence);
     });
+
+    it("Parse 1 - CLIP Welcome", function() {
+
+        var line = "1 myself 1041253132 192.168.1.308\r\n";
+        var msg = fibs.parse(line);
+        console.log("msg", msg);
+        expect(msg.length).toBe(1);
+        var m = msg[0];
+        expect(m.name).toBe("CLIP Welcome");
+        expect(m.type).toBe("CLIP");
+        expect(m.id).toBe(1);
+        expect(m.trueClip).toBeTruthy();
+        expect(m.text).toBe("1 myself 1041253132 192.168.1.308");
+        expect(m.loginName).toBe("myself");
+        expect(m.lastLogin).toBe(1041253132);
+        expect(m.lastLoginDate.getTime()).toBe(new Date(1041253132000).getTime());
+        var t = m.tokenize();
+        expect(t[0]).toBe("1");
+        expect(t[1]).toBe("myself");
+        expect(t[2]).toBe("1041253132");
+        expect(t[3]).toBe("192.168.1.308");
+
+    });
 });
